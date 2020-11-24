0a018fde801635992b9e809cb6d1077511af5dee
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-11-24 17:37:18 +0800

    更新代码

--- src/main/java/study/elasticsearch/springboot/support/SpringBootCfg.java
+++ src/main/java/study/elasticsearch/springboot/support/SpringBootCfg.java
@@ -1,7 +1,6 @@
 package study.elasticsearch.springboot.support;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 @SpringBootApplication(scanBasePackages = "study.elasticsearch.springboot")
 public class SpringBootCfg {
