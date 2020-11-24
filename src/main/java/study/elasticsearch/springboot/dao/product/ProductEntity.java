d58356b6bd303e3efbb33e296b4e2ce8e77b8b9f
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-11-24 14:35:54 +0800

    更新代码

---
+++ src/main/java/study/elasticsearch/springboot/dao/product/ProductEntity.java
@@ -0,0 +1,19 @@
+package study.elasticsearch.springboot.dao.product;
+
+import lombok.Data;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+@Data
+@Document(indexName = "product", shards = 5, replicas = 1)
+public class ProductEntity {
+
+    @Id
+    private Long ubId;
+    @Field(type = FieldType.Text, analyzer = "ik_max_word")
+    private String ubLoginName;
+    @Field(type = FieldType.Text)
+    private String ubLoginPwd;
+}
