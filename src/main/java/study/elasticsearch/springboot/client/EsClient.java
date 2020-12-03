22c68b5c1d4946a99eab72408c000cfaf1422a5f
wangzhenjun3 <wangzhenjun3@xdf.cn>
2020-12-03 17:35:55 +0800

    提交

--- src/main/java/study/elasticsearch/springboot/client/EsClient.java
+++ src/main/java/study/elasticsearch/springboot/client/EsClient.java
@@ -1,11 +1,18 @@
 package study.elasticsearch.springboot.client;
 
+import org.elasticsearch.action.admin.indices.delete.DeleteIndexRequest;
 import org.elasticsearch.action.delete.DeleteRequest;
+import org.elasticsearch.action.delete.DeleteResponse;
 import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.support.master.AcknowledgedResponse;
+import org.elasticsearch.action.update.UpdateRequest;
+import org.elasticsearch.action.update.UpdateResponse;
 import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.client.indices.CreateIndexRequest;
 import org.elasticsearch.client.indices.CreateIndexResponse;
+import org.elasticsearch.rest.RestStatus;
 
 public class EsClient {
 
@@ -24,8 +31,22 @@ public class EsClient {
             CreateIndexRequest request = new CreateIndexRequest(index);
             CreateIndexResponse response = client.indices()
                     .create(request, options);
+            response.isAcknowledged();
+            response.isShardsAcknowledged();
         } catch (Exception ex) {
-            throw new RuntimeException("", ex);
+            throw new RuntimeException("create index error!", ex);
+        }
+    }
+
+    //删除索引
+    public void deleteIndex(String index, RequestOptions options) {
+        try {
+            DeleteIndexRequest request = new DeleteIndexRequest(index);
+            AcknowledgedResponse response = client.indices()
+                    .delete(request, options);
+            response.isAcknowledged();
+        } catch (Exception ex) {
+            throw new RuntimeException("create index error!", ex);
         }
     }
 
@@ -36,29 +57,30 @@ public class EsClient {
     public void insertDoc(String index, RequestOptions options) {
         try {
             IndexRequest request = new IndexRequest(index);
-            client.index(request, options);
+            IndexResponse response = client.index(request, options);
         } catch (Exception ex) {
-            throw new RuntimeException("", ex);
+            throw new RuntimeException("insert doc error!", ex);
         }
     }
 
     //更新文档
-    public void updateDoc(String index, RequestOptions options) {
+    public void updateDoc(String index, String id, RequestOptions options) {
         try {
-//            UpdateRequest request = new UpdateRequest(index);
-//            client.update(request, options);
+            UpdateRequest request = new UpdateRequest(index, id);
+            UpdateResponse response = client.update(request, options);
         } catch (Exception ex) {
-            throw new RuntimeException("", ex);
+            throw new RuntimeException("update doc error!", ex);
         }
     }
 
     //删除文档
-    public void deleteDoc(String index, RequestOptions options) {
+    public void deleteDoc(String index, String id, RequestOptions options) {
         try {
-            DeleteRequest request = new DeleteRequest(index);
-            client.delete(request, options);
+            DeleteRequest request = new DeleteRequest(index, id);
+            DeleteResponse response = client.delete(request, options);
+            RestStatus status = response.status();
         } catch (Exception ex) {
-            throw new RuntimeException("", ex);
+            throw new RuntimeException("delete doc error!", ex);
         }
     }
 }
